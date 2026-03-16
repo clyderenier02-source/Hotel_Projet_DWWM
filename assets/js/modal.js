714219f732a267cffee6da110f02da789cbb8586
Clyde <clyde.renier02@gmail.com>
2026-03-16 13:43:20 +0100

    Add admin back office base structure

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,25 +1,22 @@
-// // Get the modal
-// var modal = document.getElementById("myModal");
+document.addEventListener("turbo:load", () => {
 
-// // Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
+    const myBtn = document.querySelectorAll(".btn");
 
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
+    myBtn.forEach(btn => {
+        btn.addEventListener("click", () => {
+            const modal = document.getElementById(btn.dataset.modal);
 
-// // When the user clicks on the button, open the modal
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
+            modal.style.display = "block";
+        })
+    });
 
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
+    const btnClose = document.querySelectorAll(".close");
 
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
+    btnClose.forEach(close => {
+        close.addEventListener("click", () => {
+            const modalContent = close.parentElement.parentElement;
+
+            modalContent.style.display = "none";
+        })
+    })
+})
