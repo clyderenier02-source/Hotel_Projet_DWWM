9f27637c8a169d3a75c35ba9e211f7154162a69b
Clyde <clyde.renier02@gmail.com>
2026-03-31 14:28:51 +0200

    feat: finalize Contact admin controller with delete and fix user redirect

---
+++ src/Controller/admin/ContactController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller\admin;
+
+use App\Entity\Contact;
+use App\Repository\ContactRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+
+#[IsGranted('ROLE_ADMIN')]
+#[Route('/admin/contact')]
+final class ContactController extends AbstractController
+{
+    #[Route('', name: 'app_admin_contact_index')]
+    public function index(ContactRepository $contactRepository): Response
+    {
+        return $this->render('admin/contact/index.html.twig', [
+            'contacts' => $contactRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_contact_show', methods: ['GET'])]
+    public function show(Contact $contact): Response
+    {
+        return $this->render('admin/contact/show.html.twig', [
+            'contact' => $contact,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_contact_delete', methods: ['POST'])]
+    public function delete(Request $request, Contact $contact, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$contact->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($contact);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_admin_contact_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
