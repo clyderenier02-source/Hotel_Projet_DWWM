9667f5ebbf21c787fa34d6a7fb77f38e12e9c222
Clyde <clyde.renier02@gmail.com>
2026-03-31 12:46:34 +0200

    feat: move Payment index to admin controller and clean user PaymentController

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -16,16 +16,6 @@ use App\Entity\Room;
 #[Route('/payment')]
 final class PaymentController extends AbstractController
 {
-    #[Route('/index', name: 'app_payment_index', methods: ['GET', 'POST'])]
-    public function index(PaymentRepository $paymentRepository): Response
-    {   
-        $payment = $paymentRepository->findAll();
-
-        return $this->render('payment/index.html.twig', [
-            'payments' => $payment
-        ]);
-    }
-
     #[Route('/{id}/pay', name: 'app_payment_new', methods: ['GET', 'POST'])]
     public function new(Request $request, Room $room, EntityManagerInterface $entityManager): Response
     {   
@@ -99,34 +89,5 @@ final class PaymentController extends AbstractController
         return $this->render('payment/show.html.twig', [
             'payment' => $payment,
         ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_payment_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Payment $payment, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(PaymentType::class, $payment);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_payment_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('payment/edit.html.twig', [
-            'payment' => $payment,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_payment_delete', methods: ['POST'])]
-    public function delete(Request $request, Payment $payment, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$payment->getId(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($payment);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('app_payment_index', [], Response::HTTP_SEE_OTHER);
     }
 }
---
+++ src/Controller/admin/PaymentController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller\admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\PaymentRepository;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_ADMIN')]
+#[Route('/admin/payment')]
+final class PaymentController extends AbstractController
+{
+    #[Route('', name: 'app_admin_payment_index')]
+    public function index(PaymentRepository $paymentRepository): Response
+    {
+        return $this->render('admin/payment/index.html.twig', [
+            'payments' => $paymentRepository->findAll()
+        ]);
+    }
+}
