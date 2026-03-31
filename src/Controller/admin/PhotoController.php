7352fb0fea6b2a0e94b290e110235b31eee70f3c
Clyde <clyde.renier02@gmail.com>
2026-03-31 11:30:41 +0200

    feat: move Photo CRUD to admin controller

--- src/Controller/admin/PhotoController.php
+++ src/Controller/admin/PhotoController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\admin;
 
 use App\Entity\Photo;
 use App\Form\PhotoType;
@@ -11,20 +11,23 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
-#[Route('/photo')]
+#[IsGranted('ROLE_ADMIN')]
+#[Route('/admin/photo')]
 final class PhotoController extends AbstractController
 {
-    #[Route(name: 'app_photo_index', methods: ['GET'])]
+    #[Route(name: 'app_admin_photo_index', methods: ['GET'])]
     public function index(PhotoRepository $photoRepository): Response
     {
-        return $this->render('photo/index.html.twig', [
+        return $this->render('admin/photo/index.html.twig', [
             'photos' => $photoRepository->findAll(),
         ]);
     }
 
-    #[Route('/new', name: 'app_photo_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/new', name: 'app_admin_photo_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $photo = new Photo();
         $form = $this->createForm(PhotoType::class, $photo);
@@ -32,49 +35,52 @@ final class PhotoController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
 
-            /** @var UploadedFile $file */
+            // Récupération du fichier depuis le formulaire
             $file = $form->get('filename')->getData();
 
+            // Si un fichier a été envoyé :
             if ($file) {
 
+            // On travail le nom du fichier
                 $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
 
-                $safeFilename = transliterator_transliterate(
-                    'Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()',
-                    $originalFilename
-                );
+                // nécessaire pour inclure le nom du fichier à une partie de l'URL de manière sécurisée
+                $safeFilename = $slugger->slug($originalFilename);
 
+                // On ajoute un identifiant unique au nom du fichier pour s'assurer que deux fichiers n'aient pas le même nom
                 $newFilename = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
 
+                // On déplace le fichier sur le serveur
                 $file->move(
                     $this->getParameter('uploads_directory'),
                     $newFilename
                 );
 
+                // On ajoute à notre objet
                 $photo->setFilename($newFilename);
             }
 
             $entityManager->persist($photo);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_photo_index');
+            return $this->redirectToRoute('app_admin_photo_index');
         }
 
-        return $this->render('photo/new.html.twig', [
+        return $this->render('admin/photo/new.html.twig', [
             'photo' => $photo,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_photo_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'app_admin_photo_show', methods: ['GET'])]
     public function show(Photo $photo): Response
     {
-        return $this->render('photo/show.html.twig', [
+        return $this->render('admin/photo/show.html.twig', [
             'photo' => $photo,
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_photo_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'app_admin_photo_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Photo $photo, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(PhotoType::class, $photo);
@@ -83,16 +89,16 @@ final class PhotoController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_photo_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_admin_photo_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('photo/edit.html.twig', [
+        return $this->render('admin/photo/edit.html.twig', [
             'photo' => $photo,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_photo_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'app_admin_photo_delete', methods: ['POST'])]
     public function delete(Request $request, Photo $photo, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$photo->getId(), $request->getPayload()->getString('_token'))) {
@@ -100,6 +106,6 @@ final class PhotoController extends AbstractController
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_photo_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_admin_photo_index', [], Response::HTTP_SEE_OTHER);
     }
 }
