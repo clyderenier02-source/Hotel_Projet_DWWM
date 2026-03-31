7ad69ca19e611cc7e28f4911c08a8bbecb0ffe33
Clyde <clyde.renier02@gmail.com>
2026-03-31 11:06:02 +0200

    feat: move Service CRUD to admin controller and clean user ServiceController

--- src/Controller/admin/ServiceController.php
+++ src/Controller/admin/ServiceController.php
@@ -2,14 +2,21 @@
 
 namespace App\Controller\admin;
 
+use App\Entity\Service;
+use App\Form\ServiceType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use App\Repository\ServiceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_ADMIN')]
+#[Route('/admin/service')]
 final class ServiceController extends AbstractController
 {
-    #[Route('/admin/service', name: 'app_admin_service')]
+    #[Route('', name: 'app_admin_service_index')]
     public function index(ServiceRepository $serviceRepository): Response
     {   
         $service = $serviceRepository->findAll();
@@ -17,5 +24,62 @@ final class ServiceController extends AbstractController
         return $this->render('admin/service/index.html.twig', [
             'services' => $service
         ]);
+    }
+
+    #[Route('/new', name: 'app_admin_service_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $service = new Service();
+        $form = $this->createForm(ServiceType::class, $service);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($service);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_admin_service_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('admin/service/new.html.twig', [
+            'service' => $service,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_service_show', methods: ['GET'])]
+    public function show(Service $service): Response
+    {
+        return $this->render('admin/service/show.html.twig', [
+            'service' => $service,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_admin_service_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Service $service, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ServiceType::class, $service);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_admin_service_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('admin/service/edit.html.twig', [
+            'service' => $service,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_service_delete', methods: ['POST'])]
+    public function delete(Request $request, Service $service, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$service->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($service);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_admin_service_index', [], Response::HTTP_SEE_OTHER);
     }
 }
